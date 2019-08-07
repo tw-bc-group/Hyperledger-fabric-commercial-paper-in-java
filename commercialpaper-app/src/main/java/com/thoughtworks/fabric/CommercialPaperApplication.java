56f5e1f0843153a8abdb8529ae513a38f8047bc3
Jin <jbjxu@thoughtworks.com>
2019-08-07 21:44:00 +0800

    feat: init project of commercialpaper-app

---
+++ commercialpaper-app/src/main/java/com/thoughtworks/fabric/CommercialPaperApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.fabric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialPaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercialPaperApplication.class, args);
+    }
+}
